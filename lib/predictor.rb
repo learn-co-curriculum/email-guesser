d8496aa1974e7528d65b42322c08e68d1da48007
Katie Hoffman <ktahoffman@gmail.com>
2014-09-17 18:57:12 -0400

    update spec error

--- lib/predictor.rb
+++ lib/predictor.rb
@@ -1,33 +1,65 @@
 require 'json'
+require 'pry'
 
 class Predictor
-  attr_reader :data
-  attr_accessor :lookup_hash
+  attr_reader :data, :lookup_hash
+  attr_accessor :first_name, :last_name
 
-  def intialize
-    @lookup_hash = {}
-    file = File.read('./dataset.json')
-    data = JSON.parse(file)
+  PATTERNS = ["full_full", "initial_full", "full_initial", "initial_initial"]
+  
+  def initialize(path_to_file)
+    file = File.read(path_to_file)
+    @data = JSON.parse(file)
+    @lookup_hash = analyze_data
+    @first_name = ""
+    @last_name = ""
+    @current_company = ""
   end
 
   def analyze_data
-
+    lookup_hash = {}
+    data.each do |full_name, email|
+      @first_name, @last_name = get_names(full_name)
+      template, domain = email.split("@")
+      lookup_hash[domain] = get_pattern(template)
+    end
+    lookup_hash
   end
 
-  def initial_last_name(name)
-
+  def guess(full_name, domain)
+    @first_name, @last_name = get_names(full_name)
+    if lookup_hash[domain]
+      address = self.send(lookup_hash[domain])
+      [address + "@" + domain]
+    else
+      PATTERNS.collect {|pattern| self.send(pattern) }
+    end
   end
 
-  def initial_initial(name)
-
+  def get_names(full_name)
+    full_name.downcase.split(" ")
   end
 
-  def first_name_initial(name)
-
+  def get_pattern(template)
+    PATTERNS.each do |pattern|
+      return pattern if self.send(pattern) == template
+    end
   end
 
-  def first_name_last_name(name)
+  def full_full
+    "#{first_name}.#{last_name}"
+  end
 
+  def initial_full
+    "#{first_name[0]}.#{last_name}"
+  end
+
+  def full_initial
+    "#{first_name}.#{last_name[0]}"
+  end
+
+  def initial_initial
+    "#{first_name[0]}.#{last_name[0]}"
   end
 
 end
--- spec/predictor_spec.rb
+++ spec/predictor_spec.rb
@@ -1,33 +1,37 @@
 describe "Predictor" do
 
-  it "can initiate a new instance of the class" do
-    expect { Predictor.new }.to_not raise_error
+  it "can initiate a new instance of the class with a path to the JSON file" do
+    expect {  Predictor.new('lib/dataset.json') }.to_not raise_error
   end
 
   describe "#guess" do
 
     before(:each) do
-      predictor = Predictor.new
+      @my_predictor =  Predictor.new('lib/dataset.json')
     end
 
     it "accepts two arguments: full name and company domain" do
-      expect(predictor.guess("Ailish Wong","wiser.com")).to_not raise_error(ArgumentError)
+      expect { @my_predictor.guess("Ailish Wong","wiser.com") }.to_not raise_error
+    end
+
+    it "returns an array" do
+      expect(@my_predictor.guess("Ailish Wong","wiser.com").class).to eq(Array)
     end
 
     it "predicts first-name-dot-last-name emails" do
-      expect(predictor.guess("Ailish Wong","wiser.com")).to eq(["ailish.wong@wiser.com"])
+      expect(@my_predictor.guess("Ailish Wong","wiser.com")).to eq(["ailish.wong@wiser.com"])
     end
 
     it "predicts first-initial-dot-last-name emails" do
-      expect(predictor.guess("Makoto Laar", "google.com")).to eq(["m.laar@google.com"])
+      expect(@my_predictor.guess("Makoto Laar", "google.com")).to eq(["m.laar@google.com"])
     end
 
     it "predicts first-initial-dot-last-initial emails" do
-      expect(predictor.guess("Fiona Condon", "etsy.com")).to eq(["f.e@etsy.com"])
+      expect(@my_predictor.guess("Fiona Condon", "etsy.com")).to eq(["f.c@etsy.com"])
     end
 
     it "results in array of four guesses when there is no data for a particualar company domain" do
-      whitehouse_guess = predictor.guess("Jamil Eady", "whitehouse.gov")
+      whitehouse_guess = @my_predictor.guess("Jamil Eady", "whitehouse.gov")
 
       expect(whitehouse_guess.class).to eq(Array)
       expect(whitehouse_guess.length).to eq(4)
